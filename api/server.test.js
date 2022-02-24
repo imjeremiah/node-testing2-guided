c1723a7b4906e9eb80e6bd6e3e0c4c9d499a5514
James Blacklock <jamesblacklock@protonmail.com>
2022-02-24 12:38:38 -0500

    committing to "lecture"

--- api/server.test.js
+++ api/server.test.js
@@ -4,8 +4,11 @@ const db = require('../data/dbConfig');
 beforeAll(async () => {
     await db.migrate.rollback();
     await db.migrate.latest();
-})
-// beforeEach()
+});
+
+beforeEach(async () => {
+    await db('hobbits').truncate();
+});
 
 test('verify we are using the correct environment', ()  => {
     expect(process.env.NODE_ENV).toBe('testing');
